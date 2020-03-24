a44a67fab0e689c77993c05b604786f2a3b14159
Jeff Graham <jgraham@dcmetrowebdevelopment.com>
2020-03-24 18:28:43 -0400

    feat: fetch geonames API

--- src/client/js/application.js
+++ src/client/js/application.js
@@ -1,6 +1,14 @@
+const userName = 'grahamj78';
+
 function handleSubmit(evt) {
-    evt.preventDefault();
-    alert('application.js exists');
+    fetch(`http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=${userName}`)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            const countryCode = data.geonames[0].countrycode;
+            document.getElementById('country-code').innerHTML = countryCode;
+        })
 }
 
 export { handleSubmit }
