913536f11f33fbea3531941aa83a82d298018fa7
Jeff Graham <jgraham@dcmetrowebdevelopment.com>
2020-03-24 14:10:14 -0400

    chore: add dev & prod mode

---
+++ webpack.dev.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html'
+        })
+    ]
+}
