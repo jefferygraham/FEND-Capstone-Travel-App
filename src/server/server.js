30f17164dcc4253b896b9f95315d8a719771392d
Jeff Graham <jgraham@dcmetrowebdevelopment.com>
2020-03-25 12:20:44 -0400

    chore: retrieve geonames response

--- src/server/server.js
+++ src/server/server.js
@@ -3,11 +3,21 @@ dotenv.config();
 
 // Require Express to run server and routes
 const express = require('express');
+
 // Start up an instance of app
 const cors = require('cors');
 const app = express();
+
 /* Middleware*/
 const bodyParser = require('body-parser');
+
+//For fetch requests
+const fetch = require("node-fetch");
+
+//Geonames API URL & parameters
+const geonamesURL = 'http://api.geonames.org/searchJSON?q=';
+const geonamesParameters = '&maxRows=10&fuzzy=0.8&';
+const geonamesUsername = `username=${process.env.USER_NAME}`
 
 
 //Here we are configuring express to use body-parser as middle-ware.
@@ -33,4 +43,19 @@ app.get('/', (req, res) => {
 app.post('/destination', (req, res) => {
     let data = req.body;
     console.log(data);
+    //parse destination from req object
+    let location = data.destination;
+    location = location.replace(/\s+/g, '');
+    console.log(location);
+
+    fetch(`${geonamesURL}${location}${geonamesParameters}${geonamesUsername}`)
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            let longitude = data.geonames[0].lng;
+            let latitude = data.geonames[0].lat;
+            let country = data.geonames[0].countryName;
+            console.log(longitude, latitude, country);
+        })
 });
